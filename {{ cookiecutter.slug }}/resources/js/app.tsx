9aecf0072a06be949a8957d69e0531ef1dd69d39
Anant Navadiya <anantnavadiya1530@gmail.com>
2026-02-25 17:19:14 +0530

    dev

--- {{ cookiecutter.slug }}/resources/js/app.tsx
+++ {{ cookiecutter.slug }}/resources/js/app.tsx
@@ -1,24 +1,38 @@
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
+{%- if cookiecutter.ui_library == 'Bootstrap' %}
+import '@/scss/app.scss';
+{%- else %}
+import '@/css/app.css'
+{%- endif %}
+
+
+import MainLayout from './layouts/MainLayout';
+import AppProvidersWrapper from './components/wrappers/AppProvidersWrapper'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
-    resolve: (name) =>
-        resolvePageComponent(
+    resolve: (name) => {
+        const page: any = await resolvePageComponent(
             `./pages/${name}.tsx`,
-            import.meta.glob('./pages/**/*.tsx'),
-        ),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
+            import.meta.glob('./pages/**/*.tsx')
+        )
 
-        root.render(
-            <StrictMode>
-                <App {...props} />
-            </StrictMode>,
-        );
-    }
+        page.default.layout ??= (page: React.ReactNode) => (
+            <AppProvidersWrapper>
+                <MainLayout>
+                    {page}
+                </MainLayout>
+            </AppProvidersWrapper>
+        )
+
+        return page
+    },
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />)
+    },
 });
