1e8f0aafc901cfb77640bd0a6ed4edaab1f6ab79
Anant Navadiya <anantnavadiya1530@gmail.com>
2026-02-26 12:32:01 +0530

    dev

--- {{ cookiecutter.slug }}/resources/views/app-react.blade.php
+++ {{ cookiecutter.slug }}/resources/views/app-react.blade.php
@@ -4,11 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
     <link rel="icon" href="/favicon.ico" sizes="any">
-    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 
     @viteReactRefresh
     @vite(['resources/js/app.tsx'])
